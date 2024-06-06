6ff5a84dcc917f6ef758cb60c470a77c61aef9b3
jeong-myeong-su <wjdaudtntn@naver.com>
2024-06-06 22:08:55 +0900

    🎉 tada: RecyclerView Test Project first commit

---
+++ app/src/main/java/com/wjdaudtn/recyclerview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.wjdaudtn.recyclerview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wjdaudtn.recyclerview.databinding.ActivityMainBinding
+import com.wjdaudtn.recyclerview.ui.theme.RecyclerViewTheme
+
+class MainActivity : AppCompatActivity(){
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
